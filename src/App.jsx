8cac7925a3ee647be3c106d3cb507bab3d00ef21
Tej749 <jargha261975@gmail.com>
2025-05-09 11:44:09 +0545

    delete & create blog

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SingleBlog from "./pages/SingleBlog";
+import CreateBlog from "./pages/CreateBlog";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/blog/:id" element={<SingleBlog />} />
+          <Route path="/create" element={<CreateBlog />} />
         </Routes>
       </BrowserRouter>
     </>
